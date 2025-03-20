84344957c00e86d9123f03076093992fcfc415be
alormada <adam.rola110@gmail.com>
2025-03-20 23:06:21 +0100

    adding sms

--- data_manager.py
+++ data_manager.py
@@ -2,12 +2,17 @@ import requests
 import os
 import flight_search
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 load_dotenv()
 
 SHEETY_ENDPOINT = os.environ["SHEETY_ENDPOINT"]
 SHEETY_TOKEN = os.environ["SHEETY_TOKEN"]
 endpoint_name = SHEETY_ENDPOINT.split("/")[-1]
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+PHONE_NUMBER = os.environ["PHONE_NUMBER"]
+TWILIO_NUMBER = os.environ["TWILIO_NUMBER"]
 
 sheety_headers = {
     "Authorization": f"Bearer {SHEETY_TOKEN}"
@@ -15,21 +20,40 @@ sheety_headers = {
 
 class DataManager:
     def __init__(self):
+        self.prices = []
         self.cities = self.get_cities()
-        self.update_iata()
+        self.update_data()
 
     def get_cities(self):
         response = requests.get(url=SHEETY_ENDPOINT, headers=sheety_headers).json()
+        print(response)
         cities = [city["city"] for city in response[endpoint_name]]
+        self.prices = [city["lowestPrice"] for city in response[endpoint_name]]
         return cities
 
-    def update_iata(self):
+    def update_data(self):
         for i in range(len(self.cities)):
             flight = flight_search.FlightSearch(self.cities[i])
-            iata = flight.get_iatacode()
+            iata = flight.iatacode
+            try:
+                best_price = flight.search_flight()
+                try:
+                    if best_price < self.prices[i]:
+                        message_text = f"Low price alert! Only {best_price} euro to fly from {flight_search.DEPARTURE_CITY} to {iata}."
+                        client = Client(account_sid, auth_token)
+                        message = client.messages.create(
+                            body=message_text,
+                            from_=TWILIO_NUMBER,
+                            to=PHONE_NUMBER
+                        )
+                except:
+                    pass
+            except:
+                best_price = "N/A"
             params = {
                 f"price": {
-                    "iataCode": f"{iata}"
+                    "iataCode": f"{iata}",
+                    "lowestPrice": f"{best_price}"
                 }
             }
             endpoint = SHEETY_ENDPOINT + f"/{i + 2}"
--- flight_search.py
+++ flight_search.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -8,6 +9,9 @@ AMADEUS_API_KEY = os.environ["AMADEUS_API_KEY"]
 AMADEUS_API_SECRET = os.environ["AMADEUS_API_SECRET"]
 BASE_URL = "test.api.amadeus.com/v1"
 CREATE_TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+days = 6 * 30
+DATES = [(datetime.now() + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(1, days + 1)]
+DEPARTURE_CITY = "WRO"
 
 headers = {
     "Content-Type": "application/x-www-form-urlencoded",
@@ -23,7 +27,9 @@ class FlightSearch:
     def __init__(self, name):
         self.cityname = name
         self.token = self.generate_new_token()
+        self.auth = {"Authorization": f"Bearer {self.token}"}
         self.iatacode = self.get_iatacode()
+        self.best_price = 10000000000000
 
     @staticmethod
     def generate_new_token():
@@ -31,8 +37,17 @@ class FlightSearch:
         return token_response["access_token"]
 
     def get_iatacode(self):
-        auth = {"Authorization": f"Bearer {self.token}"}
         city_response = requests.get(
             url=f"https://test.api.amadeus.com/v1/reference-data/locations/cities?keyword={self.cityname}&max=1",
-            headers=auth).json()
+            headers=self.auth).json()
         return city_response["data"][0]["iataCode"]
+
+    def search_flight(self):
+        for date in DATES:
+            flight_response = requests.get("https://test.api.amadeus.com/v2/shopping/"
+            f"flight-offers?originLocationCode={DEPARTURE_CITY}&destinationLocationCode={self.iatacode}"
+            f"&departureDate={date}&adults=1&nonStop=false&max=250", headers=self.auth).json()
+            total_price = float(flight_response["data"][0]["price"]["total"])
+            if total_price < self.best_price:
+                self.best_price = total_price
+            return self.best_price
