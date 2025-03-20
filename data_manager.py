6ea5b3db2cd19921b6dd9f553f39fe734cb8aea6
alormada <adam.rola110@gmail.com>
2025-03-20 01:18:14 +0100

    getting cities names from spreadsheet

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,21 @@
+import requests
+import os
+
+SHEETY_ENDPOINT = os.environ["SHEETY_ENDPOINT"]
+SHEETY_TOKEN = os.environ["SHEETY_TOKEN"]
+endpoint_name = SHEETY_ENDPOINT.split("/")[-1]
+
+sheety_headers = {
+    "Authorization": f"Bearer {SHEETY_TOKEN}"
+}
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.cities = self.get_cities()
+
+    def get_cities(self):
+        response = requests.get(url=SHEETY_ENDPOINT, headers=sheety_headers).json()
+        cities = [city["city"] for city in response[endpoint_name]]
+        return cities
+
+docs = DataManager()
